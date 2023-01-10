117c2f8f8240c93ed28bae1566c5038f8ffc514b
IsraelBosun <israelbosun1@gmail.com>
2023-01-10 19:02:17 +0100

    second-commital

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import { Aboutus, Features, Footer, Header, Sayings, Service, Solution } from './components'
 import { Navbar, Sponsors, Users } from "./oneliners"
+import "./index.css"
 
 const App = () => {
     return(
         <div>
+            <div className='right-bg'>
             <Navbar />
             <Header />
+            </div>
             <Aboutus />
             <Users />
             <Features />
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import "./header.css"
+import { FiDivide } from "react-icons/fi"
+import { AiOutlineArrowUp } from "react-icons/ai"
+
+import robo from "../../assets/robotcard.png"
 
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='container-about section'>
+      <div className = "about-flex">
+        <div className='firsthalf'>
+          <div className='twentypercent-container'>
+            <div className='icon-back'>
+              <div className=''>
+                <FiDivide className='fi' />
+              </div>
+              <h3 className = "discount">
+               <span>20%</span>  DISCOUNT FOR <span>1 MONTH </span> ACCOUNT
+              </h3>
+            </div>
+          </div>
+          <div className='next-container'>
+            <h1>The Next</h1>
+            <h1 className = "special-text-color">Generation</h1>
+            <h1 >Payment Method.</h1>
+            <h3 className= "text-color">Our team of experts uses a methodology to identify <br /> the credit cards most likely to fit your needs. <br /> We examine annual percentage rates, annual fees.</h3>
+          </div>
+        </div>
+        <div className='secondhalf'>
+          <div className='robo-div'>
+            <img src={robo} alt="robo" className = "robo" />
+          </div>
+          <div className = "gradient-blur" />
+          <div className='gradient-blur2'/>
+          <div className='gradient-blur3'/>
+        </div>
+      </div>
+      <div className='absolute'>
+          <div className='rounded'>
+            <h3 className='get'>Get <br/> Started</h3>
+            <AiOutlineArrowUp className = "arrow" />
+          </div>
+        </div>
+    </div>
   )
 }
 

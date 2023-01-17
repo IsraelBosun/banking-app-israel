a9dacd2447592e8c1a70b687f8cfae81fd9573d5
IsraelBosun <israelbosun1@gmail.com>
2023-01-17 21:11:03 +0100

    first features commital

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./features.css"
 
+import trxcard from "../../assets/transactioncard.png"
+import Content from "../../oneliners/content/Content.jsx"
+
+
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='features section__padding'>
+      <div className='features__container'>
+        <div className='transaction__card'>
+          <img src={trxcard} alt="bankcard" />
+        </div>
+        <div className='transaction__text'>
+          <Content main = "Easily control your billing & invoicing." sub = "Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat." btn = "Home" />
+        </div>
+      </div>
+    </div>
   )
 }
 

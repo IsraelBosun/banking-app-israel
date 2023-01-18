183e4c601fb50d6757d536909aa1f4819ad2e436
IsraelBosun <israelbosun1@gmail.com>
2023-01-18 20:47:43 +0100

    commit solutions

--- src/components/solution/Solution.jsx
+++ src/components/solution/Solution.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Content from "../../oneliners/content/Content.jsx"
+import aicard from "../../assets/analyticscard.png"
+
 import "./solution.css"
 
 const Solution = () => {
   return (
-    <div>Solution</div>
+    <div className='solutions section__padding'>
+        <div className='solution-text'>
+          <Content main = "Find a better card deal in few easy steps." sub = "Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau." btn = "Get Started"  />
+        </div>
+        <div className='solution-image'>
+          <img src= {aicard} alt="card" />
+        </div>
+    </div>
   )
 }
 

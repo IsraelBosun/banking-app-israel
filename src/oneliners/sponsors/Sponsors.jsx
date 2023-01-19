018394bfbca7a3cf80a210c54af25d7241155e7f
IsraelBosun <israelbosun1@gmail.com>
2023-01-19 17:39:21 +0100

    sponsors commital

--- src/oneliners/sponsors/Sponsors.jsx
+++ src/oneliners/sponsors/Sponsors.jsx
@@ -1,9 +1,32 @@
 import React from "react"
+import air from "../../assets/airbnb.png"
+import binance from "../../assets/binance.png"
+import coinbase from "../../assets/coinbase.png"
+import dropbox1 from "../../assets/dropbox1.png"
+import dropbox2 from "../../assets/dropbox2.png"
+
 import "./sponsors.css"
 
 const Sponsors = () => {
     return (
-        <div>Sponsors</div>
+        <div className = "sponsors section__padding">
+            <div className="sponsors__icons">
+                <img src={air} alt="" />
+            </div>
+
+            <div className="sponsors__icons">
+                <img src ={binance} alt="" />
+            </div>
+
+            <div className="sponsors__icons">
+                <img src={coinbase} alt="" />
+            </div>
+
+            <div className="sponsors__dropbox sponsors__icons">
+                <img src={dropbox2} alt="" />
+                <img src={dropbox1} alt="" />
+            </div>
+        </div>
     )
 }
 
